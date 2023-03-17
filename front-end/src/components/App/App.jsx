bc95856c7927a2218efaa74386d551c980830a4a
MalikMoulat <malik.moulat@hotmail.fr>
2023-03-17 12:17:03 +0100

    add home page

---
+++ front-end/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import HomePage from "../../pages/Home/home"
+
+function App() {
+    return(
+        <React.Fragment>
+            <HomePage />
+        </React.Fragment> 
+    )
+}
+
+export default App
---
+++ front-end/src/pages/Home/home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './home.css'
+
+import Header from '../../components/Header/header'
+import Banner from '../../components/Banner/banner'
+import FeatureItem from '../../components/FeatureItem/featureItem'
+import Footer from '../../components/Footer/footer'
+
+function HomePage(){
+    return(
+        <React.Fragment>
+            <Header />
+            <main>
+                <Banner />
+                <section className='features'>
+                    <h2 className="sr-only">Features</h2>
+                    <FeatureItem />
+                    <FeatureItem />
+                    <FeatureItem />
+                </section>
+            </main>
+            <Footer />
+        </React.Fragment>
+
+    )
+}
+
+export default HomePage
